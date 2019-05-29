2b0e3c31bef69620993cbcf971e718d3eb1480fb
life0727 <mmlife0727@gmail.com>
2019-05-29 11:09:40 +0800

    'logges'

--- longgest common prefix.js
+++ longgest common prefix.js
@@ -1,20 +1,30 @@
 //原题14：https://leetcode.com/problems/longest-common-prefix/
 var longestCommonPrefix = function(strs) {
-    const arr = []
-    strs.forEach(element => {
-        arr.push(element.length)
-    });
-    minl = Math.min(...arr);
-    const firststr = strs.filter(item => item.length === minl)
+    if (strs.length == 0) return ''
+    let res = '';
+    const short = strs.reduce((a,b) => a.length <= b.length ? a : b)
+    //console.log(short)
+    // const arr = []
+    // strs.forEach(element => {
+    //     arr.push(element.length)
+    // });
+    // minl = Math.min(...arr);
+    // const firststr = strs.filter(item => item.length === minl)
     //console.log(firststr)
     // for(let i = minl;i >= 0;i--){
     //     for(let j = 0; j < strs.length ; j++){
     //         if(firststr[0].substring(0,i) === strs)
     //     }
-    //     if(strs.filter(item => item.substring(0,i) === firststr[0].substring(0,i)) !== '')
-    //     console.log()
     // }
+    for(let i = short.length;i >= 0;i--){
+        if(strs.every(item => item.substring(0,i) == short.substring(0,i))){
+            res = short.substring(0,i);
+            return res
+        }
+
+    }
+    
+    return res
    
 };
-
-longestCommonPrefix(["flower","flow","flight"])
+console.log(longestCommonPrefix(["flower","flow","flight"]))
