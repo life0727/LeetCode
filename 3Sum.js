bff0f3b51db8e8a33e3f5230c6f1f5a7cde56e96
life0727 <mmlife0727@gmail.com>
2019-05-31 18:34:54 +0800

    3sum

---
+++ 3Sum.js
@@ -0,0 +1,23 @@
+//原题15：https://leetcode.com/problems/3sum/
+
+var threeSum = function(nums) {
+    if(Math.min(...nums) >= 0) return [];
+    if(Math.max(...nums) <= 0) return [];
+    let sortNums = nums.sort((a,b) => a - b);
+    
+    let positiveArr = sortNums.filter(item => item > 0);
+    let negativeArr = sortNums.filter(item => item < 0);
+
+    console.log(positiveArr)
+    console.log(negativeArr)
+    
+};
+
+const exp = [-1, 0, 1, 2, -1, -4]
+threeSum(exp)
+
+// [6,7,9,10]
+// [-1,-2,-3]
+
+// [1,2,3,4]
+// [-10,-45,-80]
