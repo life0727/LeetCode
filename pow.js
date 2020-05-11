1c4704e3268a2ebdd5e3fa9e4772605d0a3b386a
liiffe <mmlife0727@gmail.com>
2020-05-11 14:24:19 +0800

    快速幂算法 --- 分治角度

--- pow.js
+++ pow.js
@@ -1,8 +1,8 @@
 //快速幂算法 https://leetcode-cn.com/problems/powx-n/
 //实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+//例 输入: 2.00000, 10 ; 输出: 1024.00000
 
-//快速幂算法  分治思想
-
+//快速幂算法 --- 分治角度
 var myPow = function(x, n) {
     let _n = Math.abs(n);
     if(n === 0) return 1;
